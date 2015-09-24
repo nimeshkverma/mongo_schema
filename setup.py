937dfe5221bf96cc62622ff2cb51faa4318a2929
Nimesh <nimesh.aug11@gmail.com>
2015-09-24 23:01:52 +0530

    Updaing the setup file

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(name='mongoschema',
       packages=['mongoschema'],
-      version='1.0.0',
+      version='1.0',
       description='Python library for getting schema details of MongoDB collections',
       author='Nimesh Kiran, Utsav Tiwary',
       author_email='nimesh.aug11@gmail.com',
       url='https://github.com/nimeshkverma/mongo_schema',
-      download_url='',
+      download_url='https://github.com/nimeshkverma/mongo_schema/tarball/1.0',
       py_modules=['mongoschema'],
-      install_requires=['pymongo','prettytable'],
+      install_requires=['pymongo', 'prettytable'],
       keywords=['mongo', 'schema'],
       classifiers=[],
       )
